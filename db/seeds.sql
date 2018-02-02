266749cdb18da4734b9724d737b745b401a2701f
rbindas <30477545+rbindas@users.noreply.github.com>
2018-02-02 12:38:49 -0500

    initial architecture

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burgers on Brioche Buns', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Umami Burgers with Port and Stilton', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Blue Cheese Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Caesar Burger', true);
