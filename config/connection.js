ddc5c2bfa8ac78ad2998115d97b721e4f761ea85
rbindas1 <rbindas1@gmail.com>
2018-02-07 12:01:23 -0500

    working app but needs work on gui

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,27 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+
+//connection to MySQL database
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
   port: 3306,
   host: "localhost",
   user: "root",
   password: "local!access",
   database: "burgersdb"
 });
+};
+
+// var connection = mysql.createConnection({
+//   port: 3306,
+//   host: "localhost",
+//   user: "root",
+//   password: "local!access",
+//   database: "burgersdb"
+// });
 
 // Make connection.
 connection.connect(function(err) {
